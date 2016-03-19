a76799c530ee40399db422fce53c361a70a385e8
Konstantin Navrockiy <nkp2169@i.ua>
2016-03-19 23:06:44 +0200

    Update remap.py

--- remap.py
+++ remap.py
@@ -196,6 +196,10 @@ def g722(self, **words):
         COORDz0 =  coordZ[n]
         lengthZ = abs(COORDz0 - coordZ[n+1])
         lengthX = abs(COORDx0 - coordX[n+1])
+        if (COORDx0 - coordX[n+1]) >= 0:
+            pocket = 0
+        else: 
+            pocket = 1
         if lengthX == 0 :#горизонтальная линия
             delta = 0
             #l = lengthZ +l
@@ -222,8 +226,7 @@ def g722(self, **words):
                 centreX = coordX[n+1] + coordI[i]
                 centreZ = coordZ[n+1] + coordK[i]               
             i+=1 
-        print 'COORDx0=' , COORDx0  
-        print 'COORDz0=' , COORDz0                    
+                   
         while lengthZ >= 0:
             try:
                 self.execute("F%f" % feed_rate,lineno())
@@ -235,14 +238,18 @@ def g722(self, **words):
                     self.execute(" G1  X%f" % (COORDx0 + l),lineno())
                 else:
                     self.execute(" G1  X%f" % coordX_start,lineno())
-         #отходы 45гр
-                if(COORDx0 + 0.5 +l) <= 0:
-                    self.execute(" G0  X%f Z%f" % (0,(COORDz0 + 0.5 )),lineno())                       
-                elif (COORDx0 + 0.5 +l) <= coordX_start:
-                    self.execute(" G0  X%f Z%f" % ((COORDx0 + 0.5 + l),(COORDz0 + 0.5 )),lineno())# отход 45гр
-                else:
-                    self.execute(" G0  Z%f X%f" % ((COORDz0 + 0.5),(coordX_start)),lineno())# отход 45гр
+   #отходы 45гр 
+                if pocket: #для карманов
+ 
+                    if(COORDx0 + 0.5 +l) <= 0:
+                        self.execute(" G0  X%f Z%f" % (0,(COORDz0 + 0.5 )),lineno())                       
+                    elif (COORDx0 + 0.5 +l) <= coordX_start:
+                        self.execute(" G0  X%f Z%f" % ((COORDx0 + 0.5 + l),(COORDz0 + 0.5 )),lineno())# отход 45гр
+                    else:
+                        self.execute(" G0  Z%f X%f" % ((COORDz0 + 0.5),(coordX_start)),lineno())# отход 45гр
+                #else:
                     
+                
                 self.execute(" G0  X%f" % (coordX_start),lineno())# выход в стартовую по Z
                 if lengthZ < d:
                     newZ = COORDz0 - lengthZ
@@ -250,7 +257,7 @@ def g722(self, **words):
                     newZ = COORDz0 - d 
                 self.execute(" G1  Z%f" % (newZ),lineno())# новая позиция по X
                 COORDz0 = newZ
-                #просчитываем новую COORDz0 с учетом E(l) TODO пока без учета I(h):
+                #просчитываем новую COORDx0 с учетом E(l) TODO пока без учета I(h):
                 if line_or_arc[n] == 1:
                         COORDx0 = COORDx0 + delta  
                 elif line_or_arc[n] >1:
@@ -281,6 +288,132 @@ def g722(self, **words):
     f.close()                 
     return INTERP_OK
 ########################################################################################-------end G72.2
+
+#################################################-----G71.3 обработка с пазами
+def g713(self, **words):
+    """ remap code G71.3 """
+    p = int(words['p'])
+    feed_rate = int(words['f'])
+    q = int(words['q'])
+    d = float(words['d'])
+    l = float(words['k'])
+    h = float(words['i'])
+    s = linuxcnc.stat() 
+    s.poll() 
+    filename = s.file
+    f = open(filename, "r")
+    lines = f.readlines()
+    x,v,i = 0,-1,0
+    line_or_arc = []
+    coordZ = []
+    coordX = []
+    coordI = []
+    coordK = []
+    coordR = []
+    s.poll() 
+    while x < len(lines):
+        if  re.search("^\s*[(]\s*N\d", lines[x], re.I):
+            if not re.search("[^\(\)\.\-\+NGZXRIK\d\s]",lines[x].upper()):
+                num = int(re.search("N\s*([0-9.]+)",lines[x], re.I).group(1))
+                if num >= p and num <= q:
+                    v+=1
+                    g1 = line_or_arc.insert(0,(int(re.search("G\s*([0-4.]+)",lines[x], re.I).group(1))))
+                    z1 = coordZ.insert(0,(float(re.search("Z\s*([-0-9.]+)",lines[x], re.I).group(1))))
+                    x1 = coordX.insert(0,(float(re.search("X\s*([-0-9.]+)",lines[x], re.I).group(1))))                    
+                    if  re.search("[I]", lines[x]):
+                        i1 = coordI.insert(0,(float(re.search("I\s*([-0-9.]+)",lines[x], re.I).group(1))))
+                        k1 = coordK.insert(0,(float(re.search("K\s*([-0-9.]+)",lines[x], re.I).group(1))))
+                    if  re.search("[R]", lines[x]):
+                        r1 = coordR.insert(0,(float(re.search("R\s*([-0-9.]+)",lines[x], re.I).group(1))))                        
+                if num == p : # вычисляем Start_point по Z
+                    temp_x = x
+                    a=2
+                    while not re.search("^\s*.*Z", lines[temp_x-a].upper()):
+                        a+=1
+                    coordZ_start = float(re.search("Z\s*([-0-9.]+)",lines[temp_x-a], re.I).group(1))
+   
+        x+=1
+
+    for n in range(v):
+        COORDx0 =  coordX[n]
+        COORDz0 =  coordZ[n]
+        lengthZ = abs(COORDz0 - coordZ[n+1])
+        lengthX = abs(COORDx0 - coordX[n+1])
+        if lengthX == 0 :#горизонтальная линия
+            delta = 0
+            l = lengthZ +l
+        elif lengthZ == 0 : #вертикальная линия
+            delta = 0
+            l = float(words['k'])
+        else:  
+            tan = lengthX/lengthZ
+            delta = d/tan
+            height_l = l*tan
+            l = float(words['k'])
+#            if  height_l < h:
+#                l = h/tan
+            if  tan < 0.3:
+                l = 2l                         
+        if line_or_arc[n] > 1:
+            if len(coordR) :
+                pass
+                #radius = coordR[i]
+            else:
+                radius = sqrt((coordK[i])*(coordK[i]) + (coordI[i])*(coordI[i]))
+                centreX = coordX[n+1] + coordI[i]
+                centreZ = coordZ[n+1] + coordK[i]               
+            i+=1                      
+        while lengthX >= 0:
+            try:
+                self.execute("F%f" % feed_rate,lineno())
+                self.execute("G21 G18",lineno())
+                self.execute("G61",lineno())
+                if (COORDz0 + l) <= coordZ_start:                             
+                    self.execute(" G1  Z%f" % (COORDz0 + l),lineno())
+                else:
+                    self.execute(" G1  Z%f" % coordZ_start,lineno())
+                if (COORDz0 + 0.5 +l) <= coordZ_start:
+                    self.execute(" G0  X%f Z%f" % ((COORDx0 + 0.5),(COORDz0 + 0.5 + l)),lineno())# отход 45гр
+               # else:
+                    #self.execute(" G0  X%f Z%f" % ((COORDx0 + 0.5),(coordZ_start)),lineno())# отход 45гр
+                self.execute(" G0  Z%f" % (coordZ_start),lineno())# выход в стартовую по Z
+                if lengthX < d:
+                    newX = COORDx0 - lengthX
+                else:
+                    newX = COORDx0 - d 
+                self.execute(" G1  X%f" % (newX),lineno())# новая позиция по X
+                COORDx0 = newX
+                #просчитываем новую COORDz0 с учетом E(l) TODO пока без учета I(h):
+                if line_or_arc[n] == 1:
+                        COORDz0 = COORDz0 + delta  
+                elif line_or_arc[n] >1:
+                    b2 = sqrt(radius*radius - ((centreX-COORDx0)-d)*((centreX-COORDx0)-d))
+                    b1 = sqrt(radius*radius - (centreX-COORDx0)*(centreX-COORDx0))
+                    COORDz0 = COORDz0 + (abs(b2-b1)) # (abs(b2-b1)) - приращение по Z                              
+                lengthX = lengthX - d #d - съем за один проход
+            except InterpreterException,e:
+                msg = "%d: '%s' - %s" % (e.line_number,e.line_text, e.error_message)
+                self.set_errormsg(msg) 
+                return INTERP_ERROR    
+    for w in lines:
+        if  re.search("^\s*[(]\s*N\d", w.upper()):
+            if not re.search("[^\(\)\.\-\+NGZXRIK\d\s]", w.upper()):
+                num2 = int(re.findall("^\s*\d*",(re.split('N',w.upper())[1]))[0])
+                if num2 >= p and num2 <= q:
+                    try: 
+                        contour=re.split('\)',(re.split('\(',w.upper())[1]))[0]
+                        self.execute(contour,lineno())
+                    except InterpreterException,e:
+                        msg = "%d: '%s' - %s" % (e.line_number,e.line_text, e.error_message)
+                        self.set_errormsg(msg) 
+                        return INTERP_ERROR   
+    self.execute("G91",lineno())
+    self.execute(" G0  X0.5 Z0.5",lineno())# отход 45гр 
+    self.execute("G90",lineno())
+    self.execute(" G0  Z%f" % (coordZ_start),lineno())# выход в стартовую по Z                              
+    f.close()                 
+    return INTERP_OK
+    ########################################################################################-------end G71.3
 def involute(self, **words):
     """ remap function with raw access to Interpreter internals """
 
