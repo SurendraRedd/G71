2c32176a5900296e0d36d9c6caa9cc5f01c0df23
Konstantin Navrockiy <nkp2169@i.ua>
2016-03-23 10:13:32 +0200

    Update remap.py

--- remap.py
+++ remap.py
@@ -18,7 +18,13 @@ def g886(self, **words):
         MESSAGE("the P word was present")
     MESSAGE("comment on this line: '%s'" % (self.blocks[self.remap_level].comment))
     return INTERP_OK
+    
+def pars(array,reg ,lines):
+    a=array.insert(0,(float(re.search(reg,lines, re.I).group(1))))
 
+def cathetus(c,b):
+    a = sqrt(abs(c*c - b*b))
+    return a   
 #################################################-----G71.2
 def g711(self, **words):
     """ remap code G71.1 """
@@ -28,8 +34,11 @@ def g711(self, **words):
     d = float(words['d'])
     l = float(words['k'])
     h = float(words['i'])
+    #tool = float(words['t'])
     s = linuxcnc.stat() 
-    s.poll() 
+    s.poll()
+    #backangle  =  s.tool_table #TODO проверка угла резца
+    #frontangle =  s.tool_table
     filename = s.file
     f = open(filename, "r")
     lines = f.readlines()
@@ -48,27 +57,34 @@ def g711(self, **words):
                 if num >= p and num <= q:
                     v+=1
                     g1 = line_or_arc.insert(0,(int(re.search("G\s*([0-4.]+)",lines[x], re.I).group(1))))
-                    z1 = coordZ.insert(0,(float(re.search("Z\s*([-0-9.]+)",lines[x], re.I).group(1))))
-                    x1 = coordX.insert(0,(float(re.search("X\s*([-0-9.]+)",lines[x], re.I).group(1))))                    
+                    z1 = pars(coordZ,"Z\s*([-0-9.]+)",lines[x])
+                    x1 = pars(coordX,"X\s*([-0-9.]+)",lines[x])                    
                     if  re.search("[I]", lines[x]):
-                        i1 = coordI.insert(0,(float(re.search("I\s*([-0-9.]+)",lines[x], re.I).group(1))))
-                        k1 = coordK.insert(0,(float(re.search("K\s*([-0-9.]+)",lines[x], re.I).group(1))))
+                        i1 = pars(coordI,"I\s*([-0-9.]+)",lines[x])
+                        k1 = pars(coordK,"K\s*([-0-9.]+)",lines[x])
                     if  re.search("[R]", lines[x]):
-                        r1 = coordR.insert(0,(float(re.search("R\s*([-0-9.]+)",lines[x], re.I).group(1))))                        
+                        r1 = pars(coordR,"R\s*([-0-9.]+)",lines[x])                        
                 if num == p : # вычисляем Start_point по Z
                     temp_x = x
                     a=2
                     while not re.search("^\s*.*Z", lines[temp_x-a].upper()):
                         a+=1
-                    coordZ_start = float(re.search("Z\s*([-0-9.]+)",lines[temp_x-a], re.I).group(1))
-   
+                    coordZ_start = float(re.search("Z\s*([-0-9.]+)",lines[temp_x-a], re.I).group(1))   
         x+=1
-
+      
+ ######################################################
+    print 'coordZ=' , coordZ
+    print 'coordX=' , coordX              
     for n in range(v):
+        print 'n=' , n
         COORDx0 =  coordX[n]
         COORDz0 =  coordZ[n]
         lengthZ = abs(COORDz0 - coordZ[n+1])
         lengthX = abs(COORDx0 - coordX[n+1])
+        if (COORDz0 - coordZ[n+1]) >= 0:# наклон "отрицательный"
+            incline = 1   #TODO проверка угла резца
+        else: 
+            incline = 0
         if lengthX == 0 :#горизонтальная линия
             delta = 0
             l = lengthZ +l
@@ -89,24 +105,25 @@ def g711(self, **words):
                 pass
                 #radius = coordR[i]
             else:
-                radius = sqrt((coordK[i])*(coordK[i]) + (coordI[i])*(coordI[i]))
+                radius = cathetus(coordK[i],coordI[i])
                 centreX = coordX[n+1] + coordI[i]
                 centreZ = coordZ[n+1] + coordK[i]               
-            i+=1                      
-        while lengthX >= 0:
+            i+=1
+ 
+        while lengthX >= 0  :
             try:
                 self.execute("F%f" % feed_rate,lineno())
                 self.execute("G21 G18",lineno())
                 self.execute("G61",lineno())
                 if (COORDz0 + l) <= coordZ_start:                             
-                    self.execute(" G1  Z%f" % (COORDz0 + l),lineno())
+                    self.execute(" G1 Z%f" % (COORDz0 + l),lineno())
                 else:
-                    self.execute(" G1  Z%f" % coordZ_start,lineno())
+                    self.execute(" G1 Z%f" % coordZ_start,lineno())
                 if (COORDz0 + 0.5 +l) <= coordZ_start:
-                    self.execute(" G0  X%f Z%f" % ((COORDx0 + 0.5),(COORDz0 + 0.5 + l)),lineno())# отход 45гр
+                    self.execute(" G0 X%f Z%f" % ((COORDx0+0.5),(COORDz0+0.5+l)),lineno())# отход 45гр
                # else:
                     #self.execute(" G0  X%f Z%f" % ((COORDx0 + 0.5),(coordZ_start)),lineno())# отход 45гр
-                self.execute(" G0  Z%f" % (coordZ_start),lineno())# выход в стартовую по Z
+                self.execute(" G0 Z%f" % (coordZ_start),lineno())# выход в стартовую по Z
                 if lengthX < d:
                     newX = COORDx0 - lengthX
                 else:
@@ -115,16 +132,21 @@ def g711(self, **words):
                 COORDx0 = newX
                 #просчитываем новую COORDz0 с учетом E(l) TODO пока без учета I(h):
                 if line_or_arc[n] == 1:
-                        COORDz0 = COORDz0 + delta  
+                    if incline:
+                        COORDz0 = COORDz0 - delta 
+                    else:   
+                        COORDz0 = COORDz0 + delta 
                 elif line_or_arc[n] >1:
-                    b2 = sqrt(radius*radius - ((centreX-COORDx0)-d)*((centreX-COORDx0)-d))
-                    b1 = sqrt(radius*radius - (centreX-COORDx0)*(centreX-COORDx0))
+                    b2 = cathetus(radius,((centreX-COORDx0)-d))
+                    b1 = cathetus(radius,(centreX-COORDx0))
                     COORDz0 = COORDz0 + (abs(b2-b1)) # (abs(b2-b1)) - приращение по Z                              
                 lengthX = lengthX - d #d - съем за один проход
             except InterpreterException,e:
                 msg = "%d: '%s' - %s" % (e.line_number,e.line_text, e.error_message)
                 self.set_errormsg(msg) 
-                return INTERP_ERROR    
+                return INTERP_ERROR 
+                
+       
     for w in lines:
         if  re.search("^\s*[(]\s*N\d", w.upper()):
             if not re.search("[^\(\)\.\-\+NGZXRIK\d\s]", w.upper()):
@@ -138,9 +160,9 @@ def g711(self, **words):
                         self.set_errormsg(msg) 
                         return INTERP_ERROR   
     self.execute("G91",lineno())
-    self.execute(" G0  X0.5 Z0.5",lineno())# отход 45гр 
+    self.execute("G0  X0.5 Z0.5",lineno())# отход 45гр 
     self.execute("G90",lineno())
-    self.execute(" G0  Z%f" % (coordZ_start),lineno())# выход в стартовую по Z                              
+    self.execute("G0  Z%f" % (coordZ_start),lineno())# выход в стартовую по Z                              
     f.close()                 
     return INTERP_OK
 
