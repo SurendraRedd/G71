83b90a6e74ed2b992b0e15e8e0d312a472cf20ca
Konstantin Navrockiy <nkp2169@i.ua>
2017-04-18 15:46:09 +0300

    Update fc.py

--- G71/fc.py
+++ G71/fc.py
@@ -19,10 +19,21 @@ def main(argv):
     code = 'G01'
     k=0
     flag=0
+    n=1
     while x < len(lines):
-             
-                    
-    
+        if re.search("N\s*([0-9.]+)",lines[x], re.I):
+            numC = int(re.search("N\s*([0-9.]+)",lines[x], re.I).group(1))
+            if numC == t_P :
+                flag=1
+            if  numC == t_Q:
+                flag=0 
+                lines[x] = '(' + lines[x] 
+        if flag:
+            if not re.search("N\s*([0-9.]+)",lines[x], re.I):
+                lines[x] = '('+'N'+str(numC+n)+' ' + lines[x]                   
+                n+= 1
+            else:
+                lines[x] = '(' + lines[x]                    
         if re.search(".*\s*G71", lines[x], re.I) and not re.search(".*\s*[(]", lines[x], re.I):
             try:
                 t_U = float(re.search("U\s*([-0-9.]+)",lines[x], re.I).group(1))
@@ -45,7 +56,6 @@ def main(argv):
             except:
                 pass        
             if k:
-               # print 
                 strings += 'G71.2'+'P'+str(t_P)+'Q'+str(t_Q)+'D'+str(t_R)+'K'+str(t_W)+'L'+str(t_U)+'\n'
             k += 1          
         if re.search(".*\s*G0?0[^0-9]", lines[x], re.I):
@@ -67,7 +77,6 @@ def main(argv):
                     pass
             strings += lines[x]
             code = 'G00'
-
         elif re.search(".*\s*G0?1[^0-9]", lines[x], re.I):
             try:
                 Xcont = float(re.search("X\s*([-0-9.]+)",lines[x], re.I).group(1))
@@ -88,8 +97,7 @@ def main(argv):
             strings += lines[x]
             code = 'G03'             
         else:
-            if re.search("\s*[XZUW]",lines[x],re.I) and not re.search("\s*[G]",lines[x],re.I):
-                       
+            if re.search("\s*[XZUW]",lines[x],re.I) and not re.search("\s*[G]",lines[x],re.I):   
                 if re.search("\s*N", lines[x], re.I):
                     num = int(re.search("N\s*([0-9.]+)",lines[x], re.I).group(1))
                     lineUW = re.sub("\s*N[0-9]+","N"+str(num)+' '+ code,lines[x])
@@ -106,51 +114,18 @@ def main(argv):
                         pass
                     try:                        
                         Xcont_temp +=  Ucont
-                        lineUW = re.sub("U\s*([-0-9.]+)","X"+str(Xcont_temp)+' ',lines[x])
+                        lineUW = re.sub("U\s*([-0-9.]+)","X"+str(Xcont_temp)+' ',lineUW)
                     except:
                         pass 
                     try:                        
                         Zcont_temp +=  Wcont
-                        lineUW = re.sub("W\s*([-0-9.]+)","Z"+str(Zcont_temp)+' ',lines[x])
+                        lineUW = re.sub("W\s*([-0-9.]+)","Z"+str(Zcont_temp)+' ',lineUW)
                     except:
                         pass                                           
                 strings +=  lineUW                                               
         x+=1 
                                  
     print  strings
-    print  'M2'
-    
-def cord_cont(lines):
-    if re.search("\s*X", lines, re.I):                
-        return  float(re.search("X\s*([-0-9.]+)",lines, re.I).group(1)) 
-    if re.search("\s*Z", lines, re.I):                
-        return  float(re.search("Z\s*([-0-9.]+)",lines, re.I).group(1))     
+    print  'M2'    
 if __name__ == "__main__":
    main(sys.argv[1:])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
