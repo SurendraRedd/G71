fbc315fe1479903b7ba0952745b6d48c93fd0363
Konstantin Navrockiy <nkp2169@i.ua>
2017-04-19 22:08:05 +0300

    Update fc.py

--- G71/fc.py
+++ G71/fc.py
@@ -21,32 +21,37 @@ def main(argv):
     flag=0
     n=1
     while x < len(lines):
+        # заключаем в скобки строки контура
         if re.search("N\s*([0-9.]+)",lines[x], re.I):
             numC = int(re.search("N\s*([0-9.]+)",lines[x], re.I).group(1))
-            if numC == t_P :
-                flag=1
-            if  numC == t_Q:
-                flag=0 
-                l = re.sub("^\s+|\n|\r|\s+$", '',  lines[x] )
-                lines[x] = '(' + l+ ')'+'\n'   
+            try:
+                if numC == t_P :
+                    flag=1
+                if  numC == t_Q:
+                    flag=0 
+                    l = re.sub("^\s+|\n|\r|\s+$", '',  lines[x] )
+                    lines[x] = '(' + l+ ')'+'\n'  
+            except:
+                pass   
         if flag:
-            if not re.search("N\s*([0-9.]+)",lines[x], re.I):
                 l = re.sub("^\s+|\n|\r|\s+$", '',  lines[x] )
-                lines[x] = '('+'N'+str(numC+n)+' ' + l + ')'+'\n'                    
-                n+= 1
-            else:
-                l = re.sub("^\s+|\n|\r|\s+$", '',  lines[x] )
-                lines[x] = '(' + l+ ')'+'\n'                     
+                lines[x] = '(' + l+ ')'+'\n'
+                                     
         if re.search(".*\s*G71", lines[x], re.I) and not re.search(".*\s*[(]", lines[x], re.I):
             if k==0:
                 try:
-                    t_U = int(re.search("U\s*([-0-9.]+)",lines[x], re.I).group(1))
+                    t_U = float(re.search("U\s*([-0-9.]+)",lines[x], re.I).group(1))
                 except:
-                    pass
+                    print 't_U :something went wrong'
+                     
                 try:
                     t_R = float(re.search("R\s*([-0-9.]+)",lines[x], re.I).group(1))
                 except:
-                    pass        
+                    pass   
+                try:
+                    t_F = float(re.search("F\s*([-0-9.]+)",lines[x], re.I).group(1))
+                except:
+                    pass                           
             try:
                 t_P = int(re.search("P\s*([-0-9.]+)",lines[x], re.I).group(1))
             except:
@@ -58,16 +63,20 @@ def main(argv):
             try:
                 t_W = float(re.search("W\s*([-0-9.]+)",lines[x], re.I).group(1))
             except:
-                pass        
+                pass
+            try:
+                t_F = float(re.search("F\s*([-0-9.]+)",lines[x], re.I).group(1))
+            except:
+                pass                          
             if k:
-                strings += 'G71.2'+'P'+str(t_P)+'Q'+str(t_Q)+'D'+str(t_R)+'K'+str(t_W)+'L'+str(t_U)+'\n'
+                strings += 'G71'+'P'+str(t_P)+'Q'+str(t_Q)+'D'+str(t_U)+'K'+str(t_W)+'F'+str(t_F)+'\n'
             k += 1          
         if re.search(".*\s*G0?0[^0-9]", lines[x], re.I):
             try:
                 Xcont = float(re.search("X\s*([-0-9.]+)",lines[x], re.I).group(1))
                 Xcont_temp +=  Ucont               
             except:
-                pass
+                pass                
             try:
                 Zcont = float(re.search("Z\s*([-0-9.]+)",lines[x], re.I).group(1)) 
                 Zcont_temp = Zcont               
@@ -100,33 +109,31 @@ def main(argv):
         elif re.search("\s*G0?3[^0-9]", lines[x], re.I):
             strings += lines[x]
             code = 'G03'             
-        else:
-            if re.search("\s*[XZUW]",lines[x],re.I) and not re.search("\s*[G]",lines[x],re.I):   
-                if re.search("\s*N", lines[x], re.I):
-                    num = int(re.search("N\s*([0-9.]+)",lines[x], re.I).group(1))
-                    lineUW = re.sub("\s*N[0-9]+","N"+str(num)+' '+ code,lines[x])
-                else:
-                    lineUW = code + ' ' + lines[x] 
-                if re.search("\s*[UW]",lines[x],re.I):
-                    try:
-                        Ucont = float(re.search("U\s*([-0-9.]+)",lines[x], re.I).group(1))
-                    except:
-                        pass
-                    try:
-                        Wcont = float(re.search("W\s*([-0-9.]+)",lines[x], re.I).group(1))
-                    except:
-                        pass
-                    try:                        
-                        Xcont_temp +=  Ucont
-                        lineUW = re.sub("U\s*([-0-9.]+)","X"+str(Xcont_temp)+' ',lineUW)
-                    except:
-                        pass 
-                    try:                        
-                        Zcont_temp +=  Wcont
-                        lineUW = re.sub("W\s*([-0-9.]+)","Z"+str(Zcont_temp)+' ',lineUW)
-                    except:
-                        pass                                           
-                strings +=  lineUW                                               
+
+        if re.search("\s*[XZUW]",lines[x],re.I) and not re.search("\s*[G]",lines[x],re.I):    
+            if re.search("\s*[UW]",lines[x],re.I):
+                try:
+                    Ucont = float(re.search("U\s*([-0-9.]+)",lines[x], re.I).group(1))
+                except:
+                    pass
+                try:
+                    Wcont = float(re.search("W\s*([-0-9.]+)",lines[x], re.I).group(1))
+                except:
+                    pass
+                try:                        
+                    Xcont_temp +=  Ucont
+                    lineUW = re.sub("U\s*([-0-9.]+)","X"+str(Xcont_temp)+' ',lines[x])
+                except:
+                    pass 
+                try:                        
+                    Zcont_temp +=  Wcont
+                    lineUW = re.sub("W\s*([-0-9.]+)","Z"+str(Zcont_temp)+' ',lines[x])
+                except:
+                    pass                                           
+                strings +=  lineUW  
+            strings +=  lines[x] 
+        if  not re.search("[ZXUW]",lines[x].upper()): 
+            strings +=  lines[x]                                                   
         x+=1 
     f=open('fc.ngc','w')
     f.write(strings) 
